ec491292473e2e94cc779defb3fbf75dffcad580
Gourav Singh <singhgourav7217@gmail.com>
2026-04-12 12:48:49 +0530

    first push

---
+++ day1/more_basics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name = gourav
+
+echo "name is " $name
+
+# taking inp from user
+
+read username
+
+echo "username is :" $username
+
+# ading  from from like enter your username as we do in oy=ther languages
+
+read -p "enter the username:" $user
+
