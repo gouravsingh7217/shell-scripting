129d91731a6d2c2da286efd49307a1a33d5bf589
Gourav Singh <singhgourav7217@gmail.com>
2026-04-15 20:06:21 +0530

    new data

--- day1/more_basics.sh
+++ day1/more_basics.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-name = gourav
+name=gourav
 
-echo "name is " $name
+echo "name is :$name"
 
 # taking inp from user
 
 read username
 
-echo "username is :" $username
+echo "username is : $username"
 
 # ading  from from like enter your username as we do in oy=ther languages
 
-read -p "enter the username:" $user
+read -p "enter the username:" user
 
